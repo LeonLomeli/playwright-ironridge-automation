e1ddc7df0cd0893a21b7d133612e12c20ddb448a
LeonLomeli <yahirleonardo.lomeli@gmail.com>
2026-06-08 00:25:18 -0700

    New login: improvements and new code to avoid Microsoft B2C auth

--- tests/ironridge-design.spec.js
+++
@@ -1,7 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('IronRidge - Login exitoso y creación de proyecto solar', async ({ page }) => {
-  // Aquí es donde la magia va a comenzar en nuestra próxima sesión
-  await page.goto('https://base.ironridge.com/'); 
-  console.log('¡Nuevo repositorio listo para el asistente de diseño solar!');
-});
